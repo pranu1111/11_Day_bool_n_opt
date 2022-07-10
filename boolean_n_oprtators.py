3c5d3b5d4789d8e73113ec979fb449003f38dc12
Pranita kamble <107885176+pranu1111@users.noreply.github.com>
2022-07-11 00:04:44 +0530

    Add files via upload

---
+++ boolean_n_oprtators.py
@@ -0,0 +1,46 @@
+# print(10 > 9)
+# print(10 == 9)
+# print(10 < 9)
+
+# a = 105
+# b = 33 
+# if a>b:
+#     print('a is greater than b')
+# else:
+#     print('a is smaller then b')
+# a=1   
+# print(bool('HELLO'))
+# print(bool(15))
+# print(bool())
+# print(bool(0))
+# print(bool(-1))
+# print(bool(a))
+
+# x="hello"
+# y=15
+
+# print(bool(x))
+# print(bool(y))
+
+# print(bool('hello'))
+# print(bool(15))
+# print(bool(["banana","mango","staberry","apple"]))
+# print(bool(False))
+# print (bool((None)))
+# print(bool(0))
+# print(bool(""))
+# print(bool(()))
+# print(bool([])) 
+# print(bool({}))
+
+
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+  
+  x = 200
+# print(isinstance(x, int))
